8b8be3eed4a0b5be1f96a7fa4bd42f517b275f00
daruwu <57047110+patybolo@users.noreply.github.com>
2026-02-18 15:04:31 -0300

    refactor options.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,24 +1,19 @@
 local opt = vim.opt
 
--- Cursor como bloque sólido en todos los modos
--- i = insert, v = visual, c = command, n = normal
 opt.guicursor = "n-v-c-i:block"
-
--- Recomendado para temas modernos
 opt.termguicolors = true 
-opt.background = "dark" -- o "light" si prefieres el modo claro
+opt.background = "dark"
 
--- Otras opciones útiles para empezar
-opt.number = true         -- Números de línea
-opt.relativenumber = true -- Números relativos
-opt.shiftwidth = 8        -- Tamaño de la indentación
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 8
 opt.tabstop = 8
-opt.expandtab = true      -- Espacios en lugar de tabs
+opt.expandtab = true
 
 vim.diagnostic.config({
-  virtual_text = true, -- Esto muestra el error en texto tenue a la derecha
+  virtual_text = true,
   signs = true,
-  update_in_insert = false, -- No molestar mientras escribes
+  update_in_insert = false,
   underline = true,
   severity_sort = true,
   float = { border = "rounded" },
